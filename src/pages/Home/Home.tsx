a464ba47313b8051c52bc885f5cf4e38cbb6a3cb
Jacielio Queiroz <cielioqueiroz@hotmail.com>
2024-04-03 19:17:56 -0300

    adicionando estilos

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,11 @@
+import NavBar from "../../componentes/NavBar/NavBar"
 import Hero from "./sections/Hero/Hero"
 
 const Home = () => {
   
   return (
     <>
+    <NavBar />
       <Hero />
     </>
   )
--- src/pages/Home/sections/Hero/Hero.tsx
+++ src/pages/Home/sections/Hero/Hero.tsx
@@ -1,17 +1,29 @@
-import { Button, Container, Grid, Typography, styled } from "@mui/material";
+import { Box, Container, Grid, Typography, styled } from "@mui/material";
 import DownloadIcon from "@mui/icons-material/Download";
 import MailOutlineIcon from "@mui/icons-material/MailOutline";
 import Avatar from "../../../../assets/images/avatar.jpeg";
+import StyledButton from './../../../../componentes/StyledButton/StyledButton';
+import { AnimatedBackground } from './../../../../componentes/AnimatedBackground/AnimatedBackground';
+
 
 const Hero = () => {
-  const StyledHero = styled("div")(() => ({
-    backgroundColor: "black",
+  const StyledHero = styled("div")(({theme}) => ({
+    backgroundColor: theme.palette.primary.main,
     height: "100vh",
+    display:"flex",
+    alignItems: "center",
+    [theme.breakpoints.up('xs')]:{
+      paddingTop: "100px",
+    },
+    [theme.breakpoints.up('md')]:{
+      paddingTop: "0",
+    }
   }));
 
-  const StyledImg = styled("img")(() => ({
-    width: "100%",
+  const StyledImg = styled("img")(({theme}) => ({
+    width: "75%",
     borderRadius: "50%",
+    border:`2px solid ${theme.palette.primary.contrastText}`
   }));
 
   return (
@@ -19,28 +31,36 @@ const Hero = () => {
       <StyledHero>
         <Container maxWidth="lg">
           <Grid container spacing={2}>
-            <Grid item xs={12} md={2}>
-              <StyledImg src={Avatar} alt="imagem do Perfil" />
+            <Grid item xs={12} md={5}>
+              <Box position="relative">
+                <Box position="absolute" width={"150%"}  top={-100} right={0}>
+                  <AnimatedBackground />
+                </Box>  
+                <Box position="relative"textAlign="center">
+                  <StyledImg src={Avatar} alt="imagem do Perfil" />                  
+                </Box>              
+              </Box>
+              
             </Grid>
-            <Grid item xs={12} md={8}>
-              <Typography color="primary" variant="h1" textAlign="center">
-                Ciélio Queiroz
-              </Typography>
-              <Typography color="primary" variant="h2" textAlign="center">
-                I'm a Web Developer
-              </Typography>
-              <Grid container display="flex" justifyContent="center">
+            <Grid item xs={12} md={7}>
+              <Typography color="primary.contrastText" variant="h1" textAlign="center" pb={2}>Ciélio Queiroz</Typography>
+              <Typography color="primary.contrastText" variant="h2" textAlign="center">I'm a Web Developer</Typography>
+              <Grid container display="flex" justifyContent="center" spacing={2} pt={3}>
                 <Grid item xs={12} md={4} display="flex" justifyContent="center">
-                  <Button>
+                  <StyledButton onClick={() => console.log("download")}>
                     <DownloadIcon />
-                    Download CV
-                  </Button>
+                      <Typography>
+                        Download CV
+                      </Typography>
+                  </StyledButton>
                 </Grid>
                 <Grid item xs={12} md={4} display="flex" justifyContent="center">
-                  <Button>
+                  <StyledButton onClick={() => console.log("contact")}>
                     <MailOutlineIcon />
-                    Contact me
-                  </Button>
+                     <Typography>
+                       Contact me                        
+                      </Typography>
+                  </StyledButton>
                 </Grid>
               </Grid>
             </Grid>
